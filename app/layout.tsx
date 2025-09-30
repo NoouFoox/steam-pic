6bf233e135ea14581d119c10c8616fd187e258cc
贺俊浩 <nougat_hx@qq.com>
2025-09-30 22:06:15 +0800

    删除未使用的导入语句

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { FlickeringGrid } from "@/components/ui/flickering-grid";
-import { html } from "motion/react-client";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
